2c117e9f5db2255c6bf43dd5288b10a4e17bcf05
Alexander <mail.alexbelov@gmail.com>
2025-01-18 12:37:38 +0200

    added public

---
+++ build/config/client-config.js
@@ -0,0 +1,12 @@
+const clientConfig = {
+  bronzeBoxId: 267,
+  silverBoxId: 268,
+  goldBoxId: 269,
+  recommendedBoxesId: [4, 5, 6, 7, 8, 9],
+  stage: 'v1',
+  url: 'https://anb.acrm.vip/api/v1',
+  imageUrl:'https://anboxme.com/image',
+  debug: false,
+  maintenance: false,
+};
+window.clientConfig = clientConfig;
